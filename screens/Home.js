638821d4c550d99e93df8772ca554b6d1d4baf54
benumim <94872055+benumim@users.noreply.github.com>
2023-08-19 19:29:19 -0300

    Add files via upload

--- screens/Home.js
+++ screens/Home.js
@@ -18,19 +18,16 @@ export default class HomeScreen extends Component {
         return (
             <View style={styles.container}>
                 <SafeAreaView style={styles.droidSafeArea} />
-                 <ImageBackground 
-                        source={require('../assets/stars.gif')} 
-                        style={styles.backgroundImage}> 
-                
+                <ImageBackground source={require('../assets/stars.gif')} style={styles.backgroundImage}>
                     <View style={styles.titleBar}>
                         <Image source={require("../assets/main-icon.png")} style={{ width: 150, height: 150 }}></Image>
                         <Text style={styles.titleText}>App</Text>
                         <Text style={styles.titleText}>Estelar</Text>
                     </View>
-                     <TouchableOpacity 
-                          style={styles.routeCard} onPress={() =>
+
+                    <TouchableOpacity style={styles.routeCard} onPress={() =>
                         this.props.navigation.navigate("SpaceCraft")
-                    }>                   
+                    }>
                         <Text style={styles.routeText}>Naves Espaciais</Text>
                         <Image source={require("../assets/space_crafts.png")} style={styles.routeImage}></Image>
                     </TouchableOpacity>
